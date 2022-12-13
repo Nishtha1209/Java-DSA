540783a5ccfaff07748a112b8b32e2d8b154caa4
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2022-12-13 23:00:53 +0530

    Add files via upload

---
+++ Recursion/BubbleSort_recursion.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BubbleSort_recursion {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        bubble(arr, arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr, int r, int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[c+1]){
+                //swap
+                int temp = arr[c];
+                arr[c]=arr[c+1];
+                arr[c+1]=temp;
+            }
+            bubble(arr,r,c+1);
+        }else{
+            bubble(arr,r-1,0);
+        }
+    }
+}
