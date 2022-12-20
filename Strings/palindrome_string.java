5bbae4d905d972bb930d8d14ea99e65e4ab3509b
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2022-12-20 20:00:38 +0530

    Add files via upload

---
+++ Strings/palindrome_string.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Locale;
+
+public class palindrome_string {
+    public static void main(String[] args) {
+        String str = "abcdcba";
+        System.out.println(palindrome(str));
+    }
+    static boolean palindrome(String str){
+        str = str.toLowerCase();
+        for(int i = 0; i<=str.length()/2; i++){
+            char start  = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
