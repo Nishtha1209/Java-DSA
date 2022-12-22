fbba704684a23545b78f1410817b02e0d63b04ec
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2022-12-22 19:01:39 +0530

    Add files via upload

---
+++ Strings/iterativeSubSet.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.lang.reflect.InaccessibleObjectException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class iterativeSubSet {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subset(arr);
+        for(List<Integer> list : ans){
+            System.out.println(list);
+        }
+    }
+    static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer .add(new ArrayList<>());
+        for(int num: arr){
+            int n = outer.size();
+            for(int i = 0; i<n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+}
