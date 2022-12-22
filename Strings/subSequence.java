d108742396fc586f1ad4f083a41d2601f085f3d2
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2022-12-22 16:38:24 +0530

    Add files via upload

---
+++ Strings/subSequence.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class subSequence {
+    public static void main(String[] args) {
+        subseq("","abc");
+        System.out.println(subseq2("", "def"));
+    }
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq( p+ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+    static ArrayList<String> subseq2(String p, String up){  //return in the form of arraylist
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseq2(p+ch, up.substring(1));
+        ArrayList<String> right = subseq2(p, up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+
+   
+}
