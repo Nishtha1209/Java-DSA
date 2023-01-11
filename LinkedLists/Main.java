d641fabd171f1cb8c852de8935f596c588a58a14
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2023-01-11 20:59:51 +0530

    Add files via upload

---
+++ LinkedLists/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    customLinkedList list = new customLinkedList();
+        list.insertFirst(3);
+        list.insertFirst(8);
+        list.insertFirst(2);
+        list.insertFirst(17);
+
+        list.display();
+    }
+}
