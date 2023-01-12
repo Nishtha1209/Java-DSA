38e76a91569db6dcadcc230a2d071ba78fb10f53
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2023-01-12 20:26:53 +0530

    Update Main.java

--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -15,5 +15,7 @@ public class Main {
         list.display();
         System.out.println(list.deleteLast());
         list.display();
+	System.out.println(list.delete(2));
+        list.display();
     }
 }
