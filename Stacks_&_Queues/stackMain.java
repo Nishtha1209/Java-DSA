61787116ccdd5429750c817e886f02bbfbbeb5a4
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2023-02-10 22:37:32 +0530

    Update stackMain.java

--- Stacks_&_Queues/stackMain.java
+++ Stacks_&_Queues/stackMain.java
@@ -2,12 +2,14 @@ package com.company;
 
 public class stackMain {
     public static void main(String[] args) throws stackException {
-        customStack stack = new customStack(5);
+//        customStack stack = new customStack(5);
+        DynamicStack stack = new DynamicStack();
         stack.push(23);
         stack.push(42);
         stack.push(11);
         stack.push(27);
         stack.push(76);
+        stack.push(99);
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
