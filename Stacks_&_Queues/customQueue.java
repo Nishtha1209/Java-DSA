7b86e2b5d1bf9cc2600c733d3d521dd3c48cc4f7
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2023-02-20 21:17:00 +0530

    Add files via upload

---
+++ Stacks_&_Queues/customQueue.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class customQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE=10;
+
+    int end = 0; //end is pointer pointing towards end
+
+    public customQueue(int size){
+        this.data = new int[size];
+    }
+    public boolean isFull(){
+        return end == data.length;
+    }
+    public boolean isEmpty(){
+        return end == 0;
+    }
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+        data[end++]=item;
+        return true;
+    }
+    public int remove() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Queue is empty");
+        }
+        int removed = data[0];
+        //shift the elements to left
+        for(int i=1; i<end; i++){
+            data[i-1]=data[i];
+        }
+        end--;
+        return removed;
+    }
+    public int front() throws Exception{ //displaying item at front
+        if(isEmpty()){
+            throw new Exception(("Queue is empty."));
+        }
+        return data[0];
+    }
+    public void display(){
+        for (int i = 0; i < end ; i++) {
+            System.out.print(data[i]+" <- ");
+        }
+        System.out.println("END");
+    }
+}
