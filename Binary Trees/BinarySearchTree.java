3893c84569684bf2a9c98628dc2b7790798d39b1
Nishtha Kaigaonkar <nishtha.kaigaonkar12@gmail.com>
2023-07-20 19:36:39 +0530

    Update BinarySearchTree.java

--- Binary Trees/BinarySearchTree.java
+++ Binary Trees/BinarySearchTree.java
@@ -59,6 +59,18 @@ public class BinarySearchTree {
             this.insert(nums[i]);
         }
     }
+    public void populateSorted(int[] nums){
+        populateSorted(nums,0,nums.length);
+    }
+    public void populateSorted(int[] nums, int start, int end){
+        if(start>=end){
+            return;
+        }
+        int mid = (start + end)/2;
+        this.insert(nums[mid]);
+        populateSorted(nums,start,mid);
+        populateSorted(nums, mid+1, end);
+    }
     public boolean balanced(){
         return balanced(root);
     }
